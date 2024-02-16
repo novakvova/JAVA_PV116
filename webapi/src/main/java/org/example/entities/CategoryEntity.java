476fcf21567fbac402c9f79c59120c37c6ec02bc
novakvova <novakvova@gmail.com>
2024-02-16 18:19:19 +0200

    Test working Crate Category

--- webapi/src/main/java/org/example/entities/CategoryEntity.java
+++ webapi/src/main/java/org/example/entities/CategoryEntity.java
@@ -3,7 +3,8 @@ package org.example.entities;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+//import java.util.Date;
 
 @Data
 @Entity
@@ -22,6 +23,5 @@ public class CategoryEntity {
     private String description;
 
     @Column(name="date_created")
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date dateCreated;
+    private LocalDateTime creationTime;
 }
