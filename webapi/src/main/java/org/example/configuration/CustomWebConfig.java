b56eb1c76a8e4aa7c70648e0a8487ee4cf3b4846
novakvova <novakvova@gmail.com>
2024-02-19 18:23:19 +0200

    Begin app crors and files

--- webapi/src/main/java/org/example/configuration/CustomWebConfig.java
+++ webapi/src/main/java/org/example/configuration/CustomWebConfig.java
@@ -1,11 +1,17 @@
 package org.example.configuration;
 
+import lombok.AllArgsConstructor;
+import org.example.storage.StorageProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+@AllArgsConstructor
+public class CustomWebConfig implements WebMvcConfigurer {
+    private final StorageProperties storageProperties;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -14,5 +20,12 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedHeaders("*")
                 .allowCredentials(true)
                 .maxAge(3600);
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        String location = storageProperties.getLocation();
+        registry.addResourceHandler("/"+location+"/**")
+                .addResourceLocations("file:"+location+"/");
     }
 }
