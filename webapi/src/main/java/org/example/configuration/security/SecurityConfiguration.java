007199aa595b5c05a16a54b02eaa7a16f902d843
novakvova <novakvova@gmail.com>
2024-03-18 20:53:23 +0200

    Roles is good

--- webapi/src/main/java/org/example/configuration/security/SecurityConfiguration.java
+++ webapi/src/main/java/org/example/configuration/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.example.constants.Roles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 @EnableWebSecurity
@@ -34,8 +36,11 @@ public class SecurityConfiguration {
                         .requestMatchers("/webjars/**").permitAll()
                         .requestMatchers("/rest-api-docs/**").permitAll()
                         .requestMatchers("/swagger-ui/**").permitAll()
-                        .requestMatchers("/api/categories/**").permitAll()//.hasAuthority(Roles.Admin)
-                        .requestMatchers("/api/products/**").permitAll()//.hasAuthority(Roles.Admin)
+                .requestMatchers("/api/categories/search").permitAll()
+                .requestMatchers("/api/categories/**").hasAuthority(Roles.Admin)
+                .requestMatchers(HttpMethod.GET,"/api/products").permitAll()
+                .requestMatchers("/api/products/**").hasAuthority(Roles.Admin)
+                        .requestMatchers("/api/products/**").hasAuthority(Roles.Admin)
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(it->it.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
@@ -45,3 +50,50 @@ public class SecurityConfiguration {
         return http.build();
     }
 }
+
+/*
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+//    private final LogoutHandler logoutHandler;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/account/**").permitAll()
+                .requestMatchers("/uploading/**").permitAll()
+                .requestMatchers("/static/**").permitAll()
+                .requestMatchers("/swagger-resources/**").permitAll()
+                .requestMatchers("/v2/api-docs/**").permitAll()
+                .requestMatchers("/webjars/**").permitAll()
+                .requestMatchers("/rest-api-docs/**").permitAll()
+                .requestMatchers("/swagger-ui/**").permitAll()
+//                .requestMatchers("/api/categories/**").permitAll()
+//                .requestMatchers(HttpMethod.GET,"/api/products").permitAll()
+                .requestMatchers("/api/categories/search").permitAll()
+                .requestMatchers("/api/categories/**").hasAuthority(Roles.Admin)
+                .requestMatchers(HttpMethod.GET,"/api/products").permitAll()
+                .requestMatchers("/api/products/**").hasAuthority(Roles.Admin)
+                .anyRequest().authenticated()
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+//                .logout()
+//                .logoutUrl("/api/v1/auth/logout")
+//                .addLogoutHandler(logoutHandler)
+//                .logoutSuccessHandler((request, response, authentication) -> SecurityContextHolder.clearContext())
+        ;
+
+        return http.build();
+    }
+}
+*/
+
+
